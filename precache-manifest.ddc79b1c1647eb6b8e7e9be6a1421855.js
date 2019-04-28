0f9f208c2fabcfb64d9b66440beafbb4cb98b0e4
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-04-28 12:18:55 +0200

    Updates

---
+++ precache-manifest.ddc79b1c1647eb6b8e7e9be6a1421855.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f730f6b66952f714375270039f5765cc",
+    "url": "/redux_intro/index.html"
+  },
+  {
+    "revision": "b9f49a82cbbc9ad351cf",
+    "url": "/redux_intro/static/js/2.932ae9f7.chunk.js"
+  },
+  {
+    "revision": "0fc7b18602285ad2f493",
+    "url": "/redux_intro/static/js/main.97a89a53.chunk.js"
+  },
+  {
+    "revision": "2f6c97522be11f85710e",
+    "url": "/redux_intro/static/js/runtime~main.5c3d4dfe.js"
+  }
+]);
